e625f8b97eff51d640b9ac2ebcac1cea0b5242ac
dprybytkov <90059126+dprybytkov@users.noreply.github.com>
2022-03-11 16:40:53 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <div id="out">111</div>
+    <h2 class="header">Hello</h2>
+    <p id="one">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae voluptas quae ullam? Corrupti ea
+        commodi, ullam
+        exercitationem saepe sequi, perspiciatis, facere tenetur nihil quidem nam accusantium consectetur ipsa impedit
+        excepturi.</p>
+    <script src='js/script.js'></script>
+</body>
+
+</html>
